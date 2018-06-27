1c0bba348ddd361f4e30a93382afbe680491b9a0
tuxp1 <tuxp1@lenovo.com>
2018-06-27 21:08:50 +0800

    [Ushare][Code]Add signleLiveEvent

---
+++ app/src/main/java/com/tustar/ushare/ui/login/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.tustar.ushare.ui.login
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import com.tustar.ushare.data.repository.UserRepository
+
+class LoginViewModel(context: Application, repo: UserRepository) : AndroidViewModel(context) {
+
+}
